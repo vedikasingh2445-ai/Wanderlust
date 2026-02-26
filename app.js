c00ed9333013eb771dd0d5e3e21b152e3c6ab39a
vedikasingh2445-ai <vedikasingh2445@gmail.com>
2026-02-26 09:12:35 +0530

    add more details

--- app.js
+++ app.js
@@ -18,28 +18,47 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req,res)=>{
     res.send("hi i am root");
 });
-
+//Index route
 app.get("/listings",async (req,res) =>{
     const alllistings  = await Listing.find({});
     res.render("listings/index.ejs" , {alllistings});
 });
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing = new Listing({
-        title : "My new Villa",
-        description : "By the beach",
-        price : 1200,
-        location : "Calangute , Goa",
-        country : "India",
-    });
-    await sampleListing.save();
-    console.log("sample was save");
-    res.send("successful");
+//New route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
 });
+//Show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+   const listing = await Listing.findById(id); 
+   res.render("listings/show",{listing});
+});
+
+//create route
+app.post("/listings",async (req, res) => {
+     const newListing  = new Listing(req.body.listing);
+     await newlisting.save;
+})
+
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My new Villa",
+//         description : "By the beach",
+//         price : 1200,
+//         location : "Calangute , Goa",
+//         country : "India",
+//     });
+//     await sampleListing.save();
+//     console.log("sample was save");
+//     res.send("successful");
+// });
 
 app.listen(8080,() =>{
      console.log("server is listening");
